a263bc3b6b9e8cbb233d94bcda54e63d35049c1b
Dave Scherler <davescherler@gmail.com>
2015-03-14 13:24:56 -0400

    adding webview changes.

--- CTTV1/Closer To Truth/Closer To Truth/videoViewController.swift
+++ CTTV1/Closer To Truth/Closer To Truth/videoViewController.swift
@@ -10,30 +10,32 @@ import UIKit
 
 class videoViewController: ViewController {
 
+    //http:www.closertotruth.com/series/how-did-our-universe-begin
+    
     @IBOutlet weak var webView: UIWebView!
+    
     @IBAction func returnToQuoteVC(sender: AnyObject) {
+        
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let url = NSURL(string: "http://www.google.com") {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+                println(response)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.webView.reload()
+                })
+            })
+            task.resume()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
