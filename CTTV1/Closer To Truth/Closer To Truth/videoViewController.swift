b1b4721a8e4c11e3494357eabf3d5f361d95b170
Alexis Saint-Jean <asaintjean@gmail.com>
2015-03-14 12:41:04 -0400

    Create a view controller for the video.

---
+++ CTTV1/Closer To Truth/Closer To Truth/videoViewController.swift
@@ -0,0 +1,39 @@
+//
+//  videoViewController.swift
+//  Closer To Truth
+//
+//  Created by Alexis Saint-Jean on 3/14/15.
+//  Copyright (c) 2015 Alexis Saint-Jean. All rights reserved.
+//
+
+import UIKit
+
+class videoViewController: ViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    @IBAction func returnToQuoteVC(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
