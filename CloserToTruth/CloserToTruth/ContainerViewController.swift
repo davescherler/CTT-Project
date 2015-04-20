cc3c2f19c8da344ab56c4bc9227fdbf1680e0368
Alexis Saint-Jean <asaintjean@gmail.com>
2015-04-20 05:54:58 -0400

    I put the JSON fetching code into QuoteModel instead of ContainerViewVC. Will have to look that today's quote data gets loaded into the quoteView, as it might be it gets created before the JSON data for it is pulled.

--- CloserToTruth/CloserToTruth/ContainerViewController.swift
+++ CloserToTruth/CloserToTruth/ContainerViewController.swift
@@ -26,81 +26,12 @@ class ContainerViewController: UIViewController, DisplayViewControllerDelegate, 
     var menuViewController: MenuViewController?
     let displayVCExpandedOffset: CGFloat = 60
     
-    
-    
-    //ALEXIS: The following variables are to store values for quotes pulled from JSON API
-    var jsonTodaysQuote: NSArray?
-    var json: NSArray?
-    var midtempData:[String] = []
-    
-    
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         println("ContainerVC: viewDidLoad()")
         
-        //ALEXIS: working on loading JSON for today's quote
-        if let url = NSURL(string: "http://www.closertotruth.com/api/todays-quote") {
-            println("MainViewVC: The json for today's quote url does exist")
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
-                if let jsonDict: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) {
-                    self.jsonTodaysQuote = jsonDict as? NSArray
-                    println("MainViewVC: json in viewDidLoad(). jsonTodaysQuote count is now \(self.jsonTodaysQuote!.count)")
-                }
-                
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    // ACTIONS TO TAKE ONCE THE DATA IS LOADED. NOTHING DONE NOW
-                    
-                    
-                    
-                    
-                    // IMPORTANT we need to reload the data we got into our table view
-//                    self.refreshQuoteOnScreen(0, origin: "Today")
-//                    // Now that the screen is refreshed with Today's quote we need to check whether that quote is one of the favorites
-//                    self.checkIfFavorite(self.quoteID)
-                    
-//                    self.updateQuoteTextAppearance()
-                })
-            })
-            task.resume()
-        }
-        
-        // working on loading JSON for all quotes
-        // JSON Trial file https://raw.githubusercontent.com/ASJ3/PlayersGame/master/API_JSON/all-quotes-changed.json
-        if let url = NSURL(string: "http://www.closertotruth.com/api/all-quotes") {
-            println("MainViewVC: The json url does exist")
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
-                if let jsonDict: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) {
-                    self.json = jsonDict as? NSArray
-                    println("MainViewVC: json in viewDidLoad(). json count is now \(self.json!.count)")
-                    
-                    // Append all the text of quotes to the midtempData array, so that we can show these quotes
-                    // in the MenuViewController's table
-                    if let jsonData = self.json {
-                        println("MainViewVC: json in viewDidLoad(). jsonData exists")
-                        for i in jsonData {
-                            if let quote = i["quote_text"] as? NSString {
-                                var cleanText = quote as String
-                                self.midtempData.append(cleanText.stringByReplacingOccurrencesOfString("&#039;", withString: "'", options: NSStringCompareOptions.LiteralSearch, range: nil))
-                            }
-                        }
-                        println("MainViewVC: json in viewDidLoad(). midtempData count is now \(self.midtempData.count)")
-                        // ALEXIS: need to load the quote into the menu
-//                        self.menu!.allQuotesData = self.midtempData
-//                        var menuVCArray = self.menu!.allQuotesData
-//                        println("MainViewVC: json in viewDidLoad(). The number of quotes in MenuViewVC's allQuotes is \(menuVCArray.count)")
-//                        self.menu!.re_filter()
-                    }
-                }
-                
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    // IMPORTANT we need to reload the data we got into our table view
-//                    self.menu!.table.reloadData()
-                })
-            })
-            task.resume()
-        }
         
         displayViewController = UIStoryboard.displayViewController()
         displayViewController.delegate = self
--- CloserToTruth/CloserToTruth/QuoteModel.swift
+++ CloserToTruth/CloserToTruth/QuoteModel.swift
@@ -11,7 +11,79 @@ import Foundation
 class QuoteModel {
     var quotes = [QuoteData]()
     
+    //ALEXIS: The following variables are to store values for quotes pulled from JSON API
+    var jsonTodaysQuote: NSArray?
+    var json: NSArray?
+    var midtempData:[String] = []
+    
+    
     init() {
+        //ALEXIS: working on loading JSON for today's quote
+        if let url = NSURL(string: "http://www.closertotruth.com/api/todays-quote") {
+            println("QuoteModel: The json for today's quote url does exist")
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+                if let jsonDict: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) {
+                    self.jsonTodaysQuote = jsonDict as? NSArray
+                    println("QuoteModel: json in viewDidLoad(). jsonTodaysQuote count is now \(self.jsonTodaysQuote!.count)")
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // ACTIONS TO TAKE ONCE THE DATA IS LOADED. NOTHING DONE NOW
+                    
+                    
+                    
+                    
+                    // IMPORTANT we need to reload the data we got into our table view
+                    //                    self.refreshQuoteOnScreen(0, origin: "Today")
+                    //                    // Now that the screen is refreshed with Today's quote we need to check whether that quote is one of the favorites
+                    //                    self.checkIfFavorite(self.quoteID)
+                    
+                    //                    self.updateQuoteTextAppearance()
+                })
+            })
+            task.resume()
+        }
+        
+        // working on loading JSON for all quotes
+        // JSON Trial file https://raw.githubusercontent.com/ASJ3/PlayersGame/master/API_JSON/all-quotes-changed.json
+        if let url = NSURL(string: "http://www.closertotruth.com/api/all-quotes") {
+            println("QuoteModel: The json url does exist")
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: { (data, response, error) -> Void in
+                if let jsonDict: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) {
+                    self.json = jsonDict as? NSArray
+                    println("QuoteModel: json in viewDidLoad(). json count is now \(self.json!.count)")
+                    
+                    // Append all the text of quotes to the midtempData array, so that we can show these quotes
+                    // in the MenuViewController's table
+                    if let jsonData = self.json {
+                        println("QuoteModel: json in viewDidLoad(). jsonData exists")
+                        for i in jsonData {
+                            if let quote = i["quote_text"] as? NSString {
+                                var cleanText = quote as String
+                                self.midtempData.append(cleanText.stringByReplacingOccurrencesOfString("&#039;", withString: "'", options: NSStringCompareOptions.LiteralSearch, range: nil))
+                            }
+                        }
+                        println("QuoteModel: json in viewDidLoad(). midtempData count is now \(self.midtempData.count)")
+                        // ALEXIS: need to load the quote into the menu
+                        //                        self.menu!.allQuotesData = self.midtempData
+                        //                        var menuVCArray = self.menu!.allQuotesData
+                        //                        println("MainViewVC: json in viewDidLoad(). The number of quotes in MenuViewVC's allQuotes is \(menuVCArray.count)")
+                        //                        self.menu!.re_filter()
+                    }
+                }
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    // IMPORTANT we need to reload the data we got into our table view
+                    //                    self.menu!.table.reloadData()
+                })
+            })
+            task.resume()
+        }
+
+        
+        
+        
+        
         // Creating dummy data. This is where the JSON code should live.
         // Essentially, the code should parse the JSON data, find all the quote data (name, text, author, url, etc) and create discrete structs of QuoteData for each set of data. We then append each struct to the array above named 'quotes.' All we need to do is have a have to iterate through all the structs created from the JSON data and add each one to the array. The dummy data is easy to add because we know we only have two structs, but in reality we may not know how many structs will be created.
         
@@ -24,7 +96,7 @@ class QuoteModel {
         var quoteTwo = QuoteData()
         quoteTwo.authorName = "Alexis"
         quoteTwo.termName = "Children"
-        quoteTwo.quoteText = "I have have children!"
+        quoteTwo.quoteText = "I have two children!"
         quotes.append(quoteTwo)
     }
     
