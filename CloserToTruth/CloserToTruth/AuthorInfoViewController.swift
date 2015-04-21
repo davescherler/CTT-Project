3637a832861c567a0c71eedb3af35ec13e95c7ad
Alexis Saint-Jean <asaintjean@gmail.com>
2015-04-21 06:48:53 -0400

    AuthorInfoVC: code for the VC is finished. Everything seems to be loading properly. Also added an loading indicator. Some of the layout needs to be updated in Storyboard.

--- CloserToTruth/CloserToTruth/AuthorInfoViewController.swift
+++ CloserToTruth/CloserToTruth/AuthorInfoViewController.swift
@@ -15,6 +15,7 @@ class AuthorInfoViewController: UIViewController {
     @IBOutlet weak var authorImage: UIImageView!
     @IBOutlet weak var authorName: UILabel!
     @IBOutlet weak var authorBio: UITextView!
+    @IBOutlet weak var loadingImageIndicator: UIActivityIndicatorView!
     
     // ALEXIS: variables to store the Author info that get passed from the DisplayVC
     var contributorID = "none"
@@ -24,6 +25,7 @@ class AuthorInfoViewController: UIViewController {
     // ALEXIS: need to call another JSON file to retrieve the author info and create a variable to store the author Image info
     var json: NSArray?
     var dataForPic: NSData?
+    var authorInfoLoaded = false
 
         let closeButtonIcon = UIImage(named: "Close Button White 4042.png")
         
@@ -40,9 +42,9 @@ class AuthorInfoViewController: UIViewController {
                         self.json = jsonArray as? NSArray
                         println("AuthorInfoVC: json in viewDidLoad(). json count is now \(self.json!.count)")
                         if let jsonInfo = self.json![0] as? NSDictionary {
-                            //                        if let authorName = jsonInfo["contributor_name"] as? NSString {
-                            //                            self.textForAuthorName = authorName
-                            //                        }
+                            //  if let authorName = jsonInfo["contributor_name"] as? NSString {
+                            //  self.textForAuthorName = authorName
+                            //  }
                             if let authorBiography = jsonInfo["contributor_bio"] as? NSString as? String {
                                 // The authorBiography comes from the server with <p></p> html tags which we need to remove
                                 // so we're using a cleanText variable to store the cleaned up version of the biography
@@ -78,8 +80,8 @@ class AuthorInfoViewController: UIViewController {
                     }
                     dispatch_async(dispatch_get_main_queue(), { () -> Void in
                         // IMPORTANT we need to reload the data we got into our table view
-//                        self.loadingImageIndicator.stopAnimating()
-//                        self.loadingImageIndicator.hidden = true
+                        self.loadingImageIndicator.stopAnimating()
+                        self.loadingImageIndicator.hidden = true
                         self.authorImage.image = UIImage(data: self.dataForPic!)
                         println("doing thread for loading the picture")
                     })
